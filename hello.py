889bfd2dcd06487cdabbe3e8f7594f5bae5919b8
Subbarao Nangineedi <subbaraonangineedi000@gmail.com>
2025-11-18 09:55:03 +0530

    Update greeting message in hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def greet():
     print("Hello, World!")
-    name = input("Enter your name: ")
+    name = input(":Hello, Alice! Welcome to Jenkins and Docker.")
     print(f"Hello, {name}! Welcome to Jenkins and Docker.")
 
 if __name__ == "__main__":
