89a3a953611ff24c22d7803d40696aa98891758a
Subbarao Nangineedi <subbaraonangineedi000@gmail.com>
2025-11-18 10:06:53 +0530

    Modify greet function to accept command line arguments

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
-def greet():
+import sys
+
+def greet(name):
     print("Hello, World!")
-    name = input(":Hello, Alice! Welcome to Jenkins and Docker.")
     print(f"Hello, {name}! Welcome to Jenkins and Docker.")
 
 if __name__ == "__main__":
-    greet()
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    else:
+        name = "Guest"  # default name if none provided
+    greet(name)
